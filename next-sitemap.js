398df284d6f69db723a2f14a2f22216ef4bc48b3
pirasanthan-jesugeevegan <pirasanth23@hotmail.com>
2022-02-23 20:59:04 +0000

    sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,9 @@
 module.exports = {
   siteUrl: 'http://pirasanth.com/',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'http://pirasanth.com/blog/how-to-set-up-cucumber-with-cypress',
+    ],
+  },
 };

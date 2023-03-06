d38a1483fcba27e03662a19f3e6509fc7724e577
Pirasanthan Jesugeevegan <pirasanthanjesugeevegan@Pirasanthans-MacBook-Pro.local>
2023-03-06 15:07:13 +0000

    updated meta tags

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -3,6 +3,7 @@ import { jsx } from 'theme-ui';
 import { useState, useEffect } from 'react';
 import { keyframes } from '@emotion/core';
 import theme from 'theme';
+import Head from 'next/head';
 import { ThemeProvider } from 'theme-ui';
 import { DrawerProvider } from '../../contexts/drawer/drawer.provider';
 import { sanityClient, urlFor } from '../../../sanity';
@@ -22,13 +23,68 @@ export default function Post({ post, author }) {
   return (
     <ThemeProvider theme={theme}>
       <DrawerProvider>
-        <SEO
-          title={post.title}
-          description={post.description}
-          author={post.author.name}
-          image={urlFor(post.mainImage)}
-          url={`https://pirasanth.com/blog/${post.slug.current}`}
-        />
+        <Head>
+          <title>{post.title}</title>
+          <meta name="description" content={post.description}></meta>
+          <meta
+            name="author"
+            content={
+              post.author.name === 'PJ'
+                ? 'Pirasanth Jesugeevegan'
+                : post.author.name
+            }
+          ></meta>
+          <meta name="title" content={post.title}></meta>
+          <meta name="image" content={urlFor(post.mainImage)}></meta>
+          {/* <!-- Google / Search Engine Tags --> */}
+          <meta itemprop="name" content={post.title}></meta>
+          <meta itemprop="description" content={post.description}></meta>
+          <meta itemprop="image" content={urlFor(post.mainImage)}></meta>
+          {/* <!-- Facebook Meta Tags --> */}
+          <meta
+            property="og:url"
+            content={`https://pirasanth.com/blog/${post.slug.current}`}
+          ></meta>
+          {/* <meta
+            property="og:site_name"
+            content={`https://pirasanth.com/blog/${post.slug.current}`}
+          ></meta> */}
+          <meta property="og:type" content="article"></meta>
+          <meta property="og:title" content={post.title}></meta>
+          <meta property="og:description" content={post.description}></meta>
+          <meta property="og:image" content={urlFor(post.mainImage)}></meta>
+          {/* <!-- Twitter Meta Tags --> */}
+          <meta
+            name="twitter:url"
+            content={`https://pirasanth.com/blog/${post.slug.current}`}
+          ></meta>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta name="twitter:title" content={post.title}></meta>
+          <meta name="twitter:description" content={post.description}></meta>
+          <meta name="twitter:image" content={urlFor(post.mainImage)}></meta>
+          <meta name="twitter:label1" content="Written by"></meta>
+          <meta
+            name="twitter:data1"
+            content={
+              post.author.name === 'PJ'
+                ? 'Pirasanth Jesugeevegan'
+                : post.author.name
+            }
+          ></meta>
+          <meta name="twitter:label2" content="Filed under"></meta>
+          <meta name="twitter:data2" content={post.keyword}></meta>
+          <meta name="twitter:site" content="https://pirasanth.com/"></meta>
+          <meta
+            name="twitter:creator"
+            content={
+              post.author.name === 'PJ'
+                ? 'Pirasanth Jesugeevegan'
+                : post.author.name
+            }
+          ></meta>
+          <meta name="og:image:width" content="2360"></meta>
+          <meta name="og:image:height" content="1380"></meta>
+        </Head>
         <Header nav={false} author={author} />
         <main>
           <BlogPost
